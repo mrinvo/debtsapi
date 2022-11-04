c766331b0b6af85e21a9f85c0b481c6d1ffe4bac
mrinvo <huss.mostafa12@gmail.com>
2022-11-04 15:03:03 +0200

    addd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -50,7 +50,7 @@ class DatabaseSeeder extends Seeder
         Article::create([
             'title' => 'first article',
             'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora a ad rerum natus officiis, enim deserunt hic, praesentium iusto libero aperiam veritatis cupiditate nulla necessitatibus fugit porro, placeat exercitationem rem?',
-            'img' => asset('upload/Frame.png'),
+            'img' => public_path('upload/Frame.png'),
 
         ]);
         Article::create([
